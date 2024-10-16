fac406b1fcf39380ffe8144f7370b5580d4e915d
jackorse <giacomo.orsenigo@gmail.com>
2024-10-16 18:00:39 +0200

    vscode config

--- main.c
+++ main.c
@@ -1,38 +1,36 @@
-#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define GROUP_SIZE 5
 
-int compare_ints(const void* a, const void* b)
+int compare_ints(const void *a, const void *b)
 {
-  int arg1 = *(const int*)a;
-  int arg2 = *(const int*)b;
+  int arg1 = *(const int *)a;
+  int arg2 = *(const int *)b;
 
   return (arg1 > arg2) - (arg1 < arg2);
 }
- 
 
-
-int median(int *A){ //constant time (propio)
-  qsort(A, GROUP_SIZE, sizeof(int), compare_ints); 
+int median(int *A)
+{ // constant time (propio)
+  qsort(A, GROUP_SIZE, sizeof(int), compare_ints);
   return A[2];
 }
 
-
-int select(int *A, int i, int n){
-  int medians[n/GROUP_SIZE];
-  for(int j =0;j < n/GROUP_SIZE; j++){
-    medians[j] = median(A+(j*GROUP_SIZE));
+int select_algorithm(int *A, int i, int n)
+{
+  int medians[n / GROUP_SIZE];
+  for (int j = 0; j < n / GROUP_SIZE; j++)
+  {
+    medians[j] = median(A + (j * GROUP_SIZE));
   }
 
-  
-
   return 0;
 }
 
-
-int main(){
-  int v[] = {7,2,4,3,5};
-  printf("median %d", median(v));
+int main()
+{
+  int v[] = {7, 2, 4, 3, 5};
+  printf("median %d\n", median(v));
   return 0;
 }
-
