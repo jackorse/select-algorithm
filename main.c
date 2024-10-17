7d3bc6a7a55b5bbe0bc0d56fe9fe5e48e5fae0ab
jackorse <giacomo.orsenigo@gmail.com>
2024-10-17 15:49:59 +0200

    array of struct

--- main.c
+++ main.c
@@ -3,15 +3,26 @@
 
 #define GROUP_SIZE 5
 
+typedef struct
+{
+  int value;
+  int *ptr;
+} int_ptr;
+
+/**
+ * Returns 1 if arg1 > arg2
+ *         0 if arg1 = arg2
+ *        -1 if arg1 < arg2
+ */
 int compare_ints(const void *a, const void *b)
 {
-  int arg1 = *(const int *)a;
-  int arg2 = *(const int *)b;
+  int_ptr arg1 = *(const int_ptr *)a;
+  int_ptr arg2 = *(const int_ptr *)b;
 
-  return (arg1 > arg2) - (arg1 < arg2);
+  return (arg1.value > arg2.value) - (arg1.value < arg2.value);
 }
 
-int median(int *A, int size)
+int_ptr median(int_ptr *A, int size)
 { // constant time (propio)
   if (size == 0)
     size = GROUP_SIZE;
@@ -19,37 +30,38 @@ int median(int *A, int size)
   return A[(size - 1) / 2];
 }
 
-void swap(int *a, int *b)
+void swap(int_ptr *a, int_ptr *b)
 {
-  int tmp = *a;
+  int_ptr tmp = *a;
   *a = *b;
   *b = tmp;
 }
 
-int partition(int *A, int n)
+int partition(int_ptr *A, int n)
 {
-  int x = A[0];
+  int x = A[0].value;
   int k = 0;
   for (int j = 1; j < n; j++)
   {
-    if (A[j] <= x)
+    if (A[j].value <= x)
     {
       k++;
       swap(&A[k], &A[j]);
     }
   }
-  swap(&A[0], &A[k]);
+  swap(A, A + k);
   return k;
 }
 
-int select_algorithm(int *A, int i, int n)
+int_ptr select_algorithm(int_ptr *A, int i, int n)
 {
   if (n <= 0)
-    return -1;
+    return (int_ptr){.value = -1, .ptr = NULL};
   if (n == 1)
-    return 0;
+    return *A;
+
   const int num_groups = n / GROUP_SIZE + (n % GROUP_SIZE == 0 ? 0 : 1);
-  int medians[num_groups];
+  int_ptr medians[num_groups];
   for (int j = 0; j < num_groups; j++)
   {
     if (j == num_groups - 1)
@@ -58,19 +70,20 @@ int select_algorithm(int *A, int i, int n)
       medians[j] = median(A + (j * GROUP_SIZE), 0);
   }
 
-  int median_of_medians = select_algorithm(medians, (num_groups - 1) / 2, num_groups);
+  int_ptr median_of_medians = select_algorithm(medians, (num_groups - 1) / 2, num_groups);
 
-  for (int j = 0; i < n; j++)
-  {
-    if (A[j] == medians[median_of_medians])
-    {
-      median_of_medians = j;
-      break;
-    }
-  }
+  // for (int j = 0; i < n; j++)
+  // {
+  //   if (A[j] == medians[median_of_medians])
+  //   {
+  //     median_of_medians = j;
+  //     break;
+  //   }
+  // }
 
-  swap(&A[0], &A[median_of_medians]);
+  swap(A, &median_of_medians);
 
+  // position of the pivot after partition
   int k = partition(A, n);
 
   if (i == k)
@@ -81,9 +94,24 @@ int select_algorithm(int *A, int i, int n)
     return select_algorithm(A + k, i - k, n - k);
 }
 
+int selection(int *A, int i, int n)
+{
+  int_ptr A_ptr[n];
+
+  for (int j = 0; j < i; j++)
+  {
+    A_ptr[j].ptr = A + j;
+    A_ptr[j].value = A[j];
+  }
+
+  int_ptr res = select_algorithm(A_ptr, i, n);
+
+  return res.value;
+}
+
 int main()
 {
   int v[] = {7, 2, 4, 3, 5, 4, 6, 8, 9, 5};
-  printf("Res %d\n", select_algorithm(v, 2, 10));
+  printf("Res %d\n", selection(v, 2, 10));
   return 0;
 }
