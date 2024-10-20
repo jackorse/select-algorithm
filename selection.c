81260b8c949cc35841a0a776a05185490dc84503
jackorse <giacomo.orsenigo@gmail.com>
2024-10-20 20:47:56 +0200

    remove pointers

--- selection.c
+++ selection.c
@@ -1,16 +1,6 @@
 #include "selection.h"
 
 /**
- * We needed to store the values of the elements in the original array, together with their relative offset in it.
- * To access them just use A[val.ptr - A], where val.ptr is the memory address of the integer and A is the array in which is stored.
- */
-typedef struct int_p
-{
-  int value;
-  struct int_p *ptr;
-} int_ptr;
-
-/**
  * Compares two int_ptr based on their value
  *
  * @param a: pointer to the first int_ptr
@@ -20,14 +10,14 @@ typedef struct int_p
  *         0 if a == b,
  *        -1 if a < b
  */
-int compare_int_ptr(const void *a, const void *b)
+int compare_ints(const void *a, const void *b)
 {
-  int_ptr arg1 = *(const int_ptr *)a;
-  int_ptr arg2 = *(const int_ptr *)b;
+  int arg1 = *(const int *)a;
+  int arg2 = *(const int *)b;
 
-  if (arg1.value < arg2.value)
+  if (arg1 < arg2)
     return -1;
-  if (arg1.value > arg2.value)
+  if (arg1 > arg2)
     return 1;
   return 0;
 }
@@ -40,13 +30,13 @@ int compare_int_ptr(const void *a, const void *b)
  *
  * @return the position of the median in the array
  */
-static inline int_ptr median(int_ptr *A, int size)
+static inline int median(int *A, int size)
 {
   if (size == 0)
     size = GROUP_SIZE;
-  qsort(A, size, sizeof(int_ptr), compare_int_ptr);
+  qsort(A, size, sizeof(int), compare_ints);
   const int offset = (size - 1) / 2;
-  return (int_ptr){.value = A[offset].value, .ptr = NULL};
+  return A[offset];
 }
 
 /**
@@ -57,9 +47,9 @@ static inline int_ptr median(int_ptr *A, int size)
  *
  * @return the int_ptr with the highest value
  */
-static inline int_ptr *max(int_ptr *a, int_ptr *b)
+static inline int max(int a, int b)
 {
-  return a->value > b->value ? a : b;
+  return a > b ? a : b;
 }
 
 /**
@@ -70,9 +60,9 @@ static inline int_ptr *max(int_ptr *a, int_ptr *b)
  *
  * @return the int_ptr with the lowest value
  */
-static inline int_ptr *min(int_ptr *a, int_ptr *b)
+static inline int min(int a, int b)
 {
-  return a->value < b->value ? a : b;
+  return a < b ? a : b;
 }
 
 /**
@@ -82,17 +72,16 @@ static inline int_ptr *min(int_ptr *a, int_ptr *b)
  *
  * @return An int_ptr struct with the median value and its position in the array A
  */
-static inline int_ptr median5(int_ptr *A)
+static inline int median5(int *A)
 {
-  int_ptr *f = max(min(A, A + 1), min(A + 2, A + 3));       // discards lowest from first 4
-  int_ptr *g = min(max(A, A + 1), max(A + 2, A + 3));       // discards biggest from first 4
-  int_ptr *res = max(min(A + 4, f), min(g, max(A + 4, f))); // median3(A[4], f, g);
-  return (int_ptr){.value = res->value, .ptr = NULL};
+  int f = max(min(A[0], A[1]), min(A[2], A[3])); // discards lowest from first 4
+  int g = min(max(A[0], A[1]), max(A[2], A[3])); // discards biggest from first 4
+  return max(min(A[4], f), min(g, max(A[4], f))); // median3(A[4], f, g);
 }
 
-static inline void swap(int_ptr *a, int_ptr *b)
+static inline void swap(int *a, int *b)
 {
-  int_ptr tmp = *a;
+  int tmp = *a;
   *a = *b;
   *b = tmp;
 }
@@ -110,12 +99,12 @@ static inline void swap(int_ptr *a, int_ptr *b)
  *
  * @return the position of the pivot after partitioning
  */
-static inline int partition(int_ptr *A, int n, int i, int x)
+static inline int partition(int *A, int n, int i, int x)
 {
   int p_index;
   for (int j = 0; j < n; j++)
   {
-    if (A[j].value == x)
+    if (A[j] == x)
     {
       p_index = j;
       break;
@@ -127,7 +116,7 @@ static inline int partition(int_ptr *A, int n, int i, int x)
   int k = 0;
   for (int j = 0; j < n - 1; j++)
   {
-    if (A[j].value < x)
+    if (A[j] < x)
     {
       swap(A + k, A + j);
       k++;
@@ -136,7 +125,7 @@ static inline int partition(int_ptr *A, int n, int i, int x)
   int kEq = k;
   for (int j = k; j < n - 1; j++)
   {
-    if (A[j].value == x)
+    if (A[j] == x)
     {
       swap(A + kEq, A + j);
       kEq++;
@@ -159,17 +148,17 @@ static inline int partition(int_ptr *A, int n, int i, int x)
  * @param i: number of values lower than the element to find
  * @param n: number of elements in the array
  */
-int_ptr select_algorithm(int_ptr *A, const int i, const int n)
+int select_algorithm(int *A, const int i, const int n)
 {
   // Error case: just return -1
   if (n <= 0 || i < 0 || i >= n)
-    return (int_ptr){.value = -1, .ptr = NULL};
+    return -1;
 
   // Base case: if n <= RECURSION_LIMIT, the recursion overhead is too high,
   // just use the naive algorithm
   if (n <= RECURSION_LIMIT)
   {
-    qsort(A, n, sizeof(int_ptr), compare_int_ptr);
+    qsort(A, n, sizeof(int), compare_ints);
     return A[i];
   }
 
@@ -178,7 +167,7 @@ int_ptr select_algorithm(int_ptr *A, const int i, const int n)
   const int num_groups = n / GROUP_SIZE + (n % GROUP_SIZE == 0 ? 0 : 1);
 
   // Find the medians for the first num_groups - 1 groups
-  int_ptr *medians = (int_ptr *)malloc(num_groups * sizeof(int_ptr));
+  int *medians = (int *)malloc(num_groups * sizeof(int));
   for (int j = 0; j < num_groups - 1; j++)
   {
     medians[j] = median5(A + (j * GROUP_SIZE));
@@ -187,7 +176,7 @@ int_ptr select_algorithm(int_ptr *A, const int i, const int n)
   medians[num_groups - 1] = median(A + ((num_groups - 1) * GROUP_SIZE), n % GROUP_SIZE);
 
   // Recursively find the median of medians
-  int_ptr median_of_medians = select_algorithm(medians, (num_groups - 1) / 2, num_groups);
+  int median_of_medians = select_algorithm(medians, (num_groups - 1) / 2, num_groups);
   free(medians);
 
   // Move the median of medians to the last position
@@ -195,7 +184,7 @@ int_ptr select_algorithm(int_ptr *A, const int i, const int n)
 
   // Partition the array around the median of medians,
   // and get the position of the median of medians (pivot)
-  int k = partition(A, n, i, median_of_medians.value);
+  int k = partition(A, n, i, median_of_medians);
 
   // If the pivot is the i-th element, return it,
   // otherwise, recursively find the i-th element in the lower or upper part
@@ -221,17 +210,5 @@ int_ptr select_algorithm(int_ptr *A, const int i, const int n)
  */
 int selection(int *A, const int i, const int n)
 {
-  // Convert the int array A to to an array of int_ptr,
-  // that can be passed to the recursive function
-  int_ptr *A_ptr = (int_ptr *)malloc(n * sizeof(int_ptr));
-
-  for (int j = 0; j < n; j++)
-  {
-    // A_ptr[j].ptr = NULL;
-    A_ptr[j].value = A[j];
-  }
-
-  int_ptr res = select_algorithm(A_ptr, i, n);
-  free(A_ptr);
-  return res.value;
+  return select_algorithm(A, i, n);
 }
